294b43f22925ad69f9794982f52faf60d29f1cce
James Grieve <jgrieve@ualberta.ca>
2022-09-12 11:20:30 -0600

    Update for end of class, API is returning HTML and needs to be fixed;

--- DotNetReact/dotnetreact_frontend/src/App.js
+++ DotNetReact/dotnetreact_frontend/src/App.js
@@ -11,32 +11,29 @@ export default class App extends Component {
     render() {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : App.renderStrings(this.state.strings);
+            : <ul>
+                {this.state.strings.map(item =>
+                    <li key={item}>{item}</li>
+                )}
+              </ul>;
 
         return (
             <div>
                 <h1 id="tabelLabel" >API Demo With Strings</h1>
                 <p>This component demonstrates interacting with a .NET API.</p>
                 {contents}
+                <button onClick={(() => { this.setState({ loading: true }); this.populateStrings(); }).bind(this)}>Refresh</button>
             </div>
         );
     }
-    /*
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
+    async populateStrings() {
+        const response = await fetch('examplereact');
+        console.log(response);
         const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
+        this.setState({ strings: data, loading: false });
     }
-    */
-    static renderStrings(strings) {
-        return (
-            <ul>
-                {strings.map(item =>
-                    <li key={item}>{item}</li>
-                )}
-            </ul>
-        );
-    }
+
+    
 
 
 
