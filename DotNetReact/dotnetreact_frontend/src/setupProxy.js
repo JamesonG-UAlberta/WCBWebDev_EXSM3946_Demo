d6b3630047e2bdfd43e2334f3e90597f7f019644
James Grieve <jgrieve@ualberta.ca>
2022-09-12 11:44:02 -0600

    Changed proxy config;

--- DotNetReact/dotnetreact_frontend/src/setupProxy.js
+++ DotNetReact/dotnetreact_frontend/src/setupProxy.js
@@ -1,7 +1,8 @@
 ﻿const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const context = [
-    "/weatherforecast",
+    "/examplereact",
+    "/ExampleReact"
 ];
 
 module.exports = function (app) {
