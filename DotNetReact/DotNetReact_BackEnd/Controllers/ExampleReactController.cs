c3901bea555a4a9ba8d2bfed5a8d3dccab8f7875
James Grieve <jgrieve@ualberta.ca>
2022-09-13 11:38:44 -0600

    Working react page displaying count with multithreaded async;

--- DotNetReact/DotNetReact_BackEnd/Controllers/ExampleReactController.cs
+++ DotNetReact/DotNetReact_BackEnd/Controllers/ExampleReactController.cs
@@ -6,6 +6,7 @@ namespace DotNetReact_BackEnd.Controllers
     [Route("[controller]")]
     public class ExampleReactController : ControllerBase
     {
+        // Serving the place of a database.
         private static List<string> Data = new List<string>();
 
         private readonly ILogger<ExampleReactController> _logger;
@@ -19,8 +20,17 @@ namespace DotNetReact_BackEnd.Controllers
         [HttpPost]
         public ActionResult Create(string newString)
         {
+            // Null - Format - Presence - BusinessRules
+            // Never Forget Peanut Butter
+
+            // Step 1 - Data is present. (N)
             if (string.IsNullOrWhiteSpace(newString)) return BadRequest("No data provided.");
+            // Step 2 - Data is in the correct format (not applicable for strings). (F)
+            // Step 3 - Data is or is not present in the database already. (P)
             if (Data.Contains(newString.Trim())) return BadRequest("Data already present.");
+            // Step 4 - Business rules (none). (B)
+
+            // Add the data.
             Data.Add(newString.Trim());
             return Ok();
         }
@@ -28,10 +38,16 @@ namespace DotNetReact_BackEnd.Controllers
         [HttpPatch]
         public ActionResult Update(string oldString, string newString)
         {
+            // Null
             if (string.IsNullOrWhiteSpace(oldString)) return BadRequest("No target data provided.");
             if (string.IsNullOrWhiteSpace(newString)) return BadRequest("No update data provided.");
+            // Format (N/A)
+            // Presence
             if (!Data.Contains(oldString.Trim())) return NotFound("Target could not be found.");
             if (Data.Contains(newString.Trim())) return BadRequest("Update data already present.");
+            // Business Rules (N/A)
+
+            // Do the update.
             Data[Data.FindIndex(x => x == oldString)] = newString.Trim();
             return Ok();
         }
@@ -39,16 +55,30 @@ namespace DotNetReact_BackEnd.Controllers
         [HttpDelete]
         public ActionResult Delete(string oldString)
         {
+            // Null
             if (string.IsNullOrWhiteSpace(oldString)) return BadRequest("No target data provided.");
+            // Format (N/A)
+            // Presence
             if (!Data.Contains(oldString.Trim())) return NotFound("Target could not be found.");
+            // Business Rules (N/A)
+
+            // Do the delete.
             Data.Remove(oldString.Trim());
             return Ok();
         }
 
         [HttpGet]
+        [Route("list")]
         public IEnumerable<string> Read()
         {
             return Data;
+        }
+
+        [HttpGet]
+        [Route("count")]
+        public int Count()
+        {
+            return Data.Count;
         }
     }
 }
--- DotNetReact/dotnetreact_frontend/src/App.js
+++ DotNetReact/dotnetreact_frontend/src/App.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 export default class App extends Component {
     static displayName = App.name;
 
+    // 1. Constructor sets a list of default strings.
     constructor(props) {
         super(props);
-        this.state = { strings: ["Hello", "World", "How do you do?"], loading: false };
+        this.state = { strings: ["Hello", "World", "How do you do?"], count:0, loadingList: false, loadingCount: false };
     }
 
+    // 2. Render the list of default strings to the page with a refresh button. Rest.
+    // 4. Render fires and sets the loading message, and awaits another state change.
+    // 6. Render fires and updates the page with the new data that has returned. Rest.
+    // If the button is clicked again, it starts again from step 3.
     render() {
-        let contents = this.state.loading
+    // Start thread A.
+        let contents = this.state.loadingList || this.state.loadingCount
             ? <p><em>Loading...</em></p>
             : <ul>
                 {this.state.strings.map(item =>
@@ -21,21 +27,38 @@ export default class App extends Component {
             <div>
                 <h1 id="tabelLabel" >API Demo With Strings</h1>
                 <p>This component demonstrates interacting with a .NET API.</p>
+                <p>There are currently {this.state.count} items stored in the server's cache.</p>
                 {contents}
-                <button onClick={(() => { this.setState({ loading: true }); this.populateStrings(); }).bind(this)}>Refresh</button>
+
+                <button onClick={(() => {
+                    // 3. When the button is clicked, set the state loading to true and begin the fetch method. Changing state triggers render to fire.
+                    this.setState({ loading: true });
+                    this.populateCount();
+                    // Start thread B.
+                    // (Thread A continues)
+                    this.populateStrings(); 
+                    // Start thread C.
+                    // (Thread A continues)
+                }).bind(this)
+
+                }>Refresh</button>
             </div>
         );
+        // Thread A ends.
     }
+
+    async populateCount() {
+        const responseCount = await fetch('examplereact/count');
+        const dataCount = await responseCount.json();
+        this.setState({ count: dataCount, loading: false });
+        // Thread B ends.
+    }
+
+    // 5. Fetch the strings and update the state with the new data and turn off loading when the data gets back.
     async populateStrings() {
-        const response = await fetch('examplereact');
-        console.log(response);
-        const data = await response.json();
-        this.setState({ strings: data, loading: false });
+        const responseList = await fetch('examplereact/list');
+        const dataList = await responseList.json();
+        this.setState({ strings: dataList, loading: false });
+        // Thread C ends.
     }
-
-    
-
-
-
-
 }
